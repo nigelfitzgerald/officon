8bc3c900196230a7f71db2e9cfabb983cbb06f3f
Philippe de Chazal <dechazalp@gmail.com>
2022-08-30 14:19:31 +0400

    booking views

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :offices
+  resources :offices do
+    resources :bookings, only: [:new, :create]
+  end
+
 end
