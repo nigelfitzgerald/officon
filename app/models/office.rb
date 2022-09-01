22499a2e8af601df3730d68d91c1fcd6fd1e2dc9
nazneen jackaria <n_jackaria@hotmail.com>
2022-09-01 16:03:20 +0400

    added search

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,3 +1,11 @@
 class Office < ApplicationRecord
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description, :address ],
+    using: {
+      tsearch: { prefix: true }
+  }
+
 end
