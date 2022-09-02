d36307ed2eb01fd38978f308b0771f261ee37752
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-02 11:12:49 +0200

    added validations and updated my bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,7 +33,7 @@ user5.save
 user6 = User.new(email: "f@live.com", password: "123456", name: "Alice")
 user6.save
 
-office1 = Office.create(surface: 100, price: 1000, description: "Nice office", title: "ExTower", address: "Citadelle Mall, Sir Virgil Naz St, Port Louis, Mauritius", user_id: user1.id, image_url: "https://assets.architecturaldigest.in/photos/6008499aa8f8397741704a19/16:9/w_2560%2Cc_limit/Ronnie-Screwvala-mumbai-RSVP-office_2-2-1366x768.jpg")
+office1 = Office.create!(surface: 100, price: 1000, description: "Nice office", title: "ExTower", address: "Citadelle Mall, Sir Virgil Naz St, Port Louis, Mauritius", user_id: user1.id, image_url: "https://assets.architecturaldigest.in/photos/6008499aa8f8397741704a19/16:9/w_2560%2Cc_limit/Ronnie-Screwvala-mumbai-RSVP-office_2-2-1366x768.jpg")
 office2 = Office.create(surface: 200, price: 1100, description: "Big office", title: "NTower", address: "1 Cybercity, Ebene, Mauritius", user_id: user2.id, image_url: "https://www.livehome3d.com/assets/img/articles/types-of-offices/modern-office-with-the-city-view.jpg")
 office3 = Office.create(surface: 300, price: 1200, description: "Small office", title: "Vivéa", address: "Vivéa Business Park, Moka, Mauritius", user_id: user3.id, image_url: "https://imageio.forbes.com/specials-images/imageserve/61576e0e6690a20767218a24/0x0.jpg?format=jpg&width=1200")
 office4 = Office.create(surface: 400, price: 1300, description: "Individual office", title: "Cybercity", address: "Cyber Tower 2 Quatre Bornes, Mauritius", user_id: user4.id, image_url: "https://images.adsttc.com/media/images/6306/642d/79c4/892c/8ba3/84bb/slideshow/shopify-offices-berlin-mvrdv_16.jpg?1661363278")
