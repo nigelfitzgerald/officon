ed47562e530e51e178499d18bed4ebe7230edb16
Nigel Fitzgerald <johnscottnigel@gmail.com>
2022-09-02 12:15:38 +0200

    updated homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @offices = Office.all
+    @offices = Office.where("address ILIKE?", "%St Pierre%")
   end
 end
